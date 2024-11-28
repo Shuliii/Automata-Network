6bf562597f083d06a583d47576f39a391006636f
Rico Harsono <ricoharsono@gmail.com>
2024-11-28 11:09:20 +0800

    move to s3 backend

--- infra/provider.tf
+++ infra/provider.tf
@@ -2,14 +2,14 @@ provider "aws" {
   region     = "ap-southeast-1"
 }
 
-#adding the s3 backend
-# terraform {
-#   backend "s3" {
-#     bucket         = "barkeley-tfstate1"
-#     key            = "terraform/statefile.tfstate"
-#     region         = "ap-southeast-1"
-#   }
-# }
+#s3 backend
+terraform {
+  backend "s3" {
+    bucket         = "rico-automata-tfstate"
+    key            = "terraform/statefile.tfstate"
+    region         = "ap-southeast-1"
+  }
+}
 
 #VPC
 resource "aws_vpc" "automata" {
