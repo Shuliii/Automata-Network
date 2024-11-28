b77b0fd256afc13b8a61377902734585005f752b
Rico Harsono <ricoharsono@gmail.com>
2024-11-28 14:52:08 +0800

    edit

--- infra/eks.tf
+++ infra/eks.tf
@@ -21,7 +21,7 @@ resource "aws_eks_node_group" "eks_node_group" {
     min_size     = 1
   }
 
-  instance_types = ["t3.micro"]
+  instance_types = ["t3.small"]
 
   depends_on = [
     aws_eks_cluster.eks_cluster
