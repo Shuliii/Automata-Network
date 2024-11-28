18464ca297e928d85e8c283cfbe4ba1c3f8a919d
Rico Harsono <ricoharsono@gmail.com>
2024-11-28 12:20:34 +0800

    edit

---
+++ infra/variable.tf
@@ -0,0 +1,21 @@
+variable "public_subnets" {
+  description = "List of public subnets with AZ and CIDR"
+  type = list(object({
+    az         = string
+    cidr_block = string
+  }))
+  default = [
+    {
+      az         = "ap-southeast-1a"
+      cidr_block = "10.0.10.0/24"
+    },
+    {
+      az         = "ap-southeast-1b"
+      cidr_block = "10.0.20.0/24"
+    },
+    {
+      az         = "ap-southeast-1c"
+      cidr_block = "10.0.30.0/24"
+    }
+  ]
+}
