fa8a0502616dc4fc3cd55201880026587a8b3c23
Rico Harsono <ricoharsono@gmail.com>
2024-12-06 15:34:59 +0800

    edit

--- infra/eks.tf
+++ infra/eks.tf
@@ -1,29 +1,29 @@
-#EKS Cluster
-resource "aws_eks_cluster" "eks_cluster" {
-  name     = "automata-cluster"
-  role_arn = aws_iam_role.eks.arn
+# #EKS Cluster
+# resource "aws_eks_cluster" "eks_cluster" {
+#   name     = "automata-cluster"
+#   role_arn = aws_iam_role.eks.arn
 
-  vpc_config {
-    subnet_ids = [for subnet in aws_subnet.public_subnet : subnet.id]
-  }
-}
+#   vpc_config {
+#     subnet_ids = [for subnet in aws_subnet.public_subnet : subnet.id]
+#   }
+# }
 
-# EKS Node group
-resource "aws_eks_node_group" "eks_node_group" {
-  cluster_name    = aws_eks_cluster.eks_cluster.name
-  node_group_name = "automata-node-group"
-  node_role_arn   = aws_iam_role.node.arn
-  subnet_ids      = [for subnet in aws_subnet.public_subnet : subnet.id]
+# # EKS Node group
+# resource "aws_eks_node_group" "eks_node_group" {
+#   cluster_name    = aws_eks_cluster.eks_cluster.name
+#   node_group_name = "automata-node-group"
+#   node_role_arn   = aws_iam_role.node.arn
+#   subnet_ids      = [for subnet in aws_subnet.public_subnet : subnet.id]
 
-  scaling_config {
-    desired_size = 1
-    max_size     = 3
-    min_size     = 1
-  }
+#   scaling_config {
+#     desired_size = 1
+#     max_size     = 3
+#     min_size     = 1
+#   }
 
-  instance_types = ["t3.small"]
+#   instance_types = ["t3.small"]
 
-  depends_on = [
-    aws_eks_cluster.eks_cluster
-  ]
-}
+#   depends_on = [
+#     aws_eks_cluster.eks_cluster
+#   ]
+# }
--- infra/provider.tf
+++ infra/provider.tf
@@ -1,67 +1,67 @@
-provider "aws" {
-  region     = "ap-southeast-1"
-}
+# provider "aws" {
+#   region     = "ap-southeast-1"
+# }
 
-#s3 backend
-terraform {
-  backend "s3" {
-    bucket     = "rico-automata-s3"
-    key        = "terraform/statefile.tfstate"
-    region     = "ap-southeast-1"
-  }
+# #s3 backend
+# terraform {
+#   backend "s3" {
+#     bucket     = "rico-automata-s3"
+#     key        = "terraform/statefile.tfstate"
+#     region     = "ap-southeast-1"
+#   }
 
-  # backend "local" {
-  #   path="terraform.tfstate"
-  # }
-}
+#   # backend "local" {
+#   #   path="terraform.tfstate"
+#   # }
+# }
 
-#VPC
-resource "aws_vpc" "automata" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "automata-vpc"
-  }
-}
+# #VPC
+# resource "aws_vpc" "automata" {
+#   cidr_block = "10.0.0.0/16"
+#   tags = {
+#     Name = "automata-vpc"
+#   }
+# }
 
-# #IGW
-resource "aws_internet_gateway" "automata-igw" {
-  vpc_id = aws_vpc.automata.id
-  tags = {
-    Name = "igw"
-  }
-}
+# # #IGW
+# resource "aws_internet_gateway" "automata-igw" {
+#   vpc_id = aws_vpc.automata.id
+#   tags = {
+#     Name = "igw"
+#   }
+# }
 
-#Route Table
-resource "aws_route_table" "public_route_table" {
-  vpc_id = aws_vpc.automata.id
-  tags = {
-    Name = "public-route-table"
-  }
-}
+# #Route Table
+# resource "aws_route_table" "public_route_table" {
+#   vpc_id = aws_vpc.automata.id
+#   tags = {
+#     Name = "public-route-table"
+#   }
+# }
 
-#Route
-resource "aws_route" "public_internet" {
-  route_table_id         = aws_route_table.public_route_table.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.automata-igw.id
-}
+# #Route
+# resource "aws_route" "public_internet" {
+#   route_table_id         = aws_route_table.public_route_table.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   gateway_id             = aws_internet_gateway.automata-igw.id
+# }
 
-#Public Subnet
-resource "aws_subnet" "public_subnet" {
-  for_each          = { for subnet in var.public_subnets : subnet.az => subnet }
-  vpc_id            = aws_vpc.automata.id
-  cidr_block        = each.value.cidr_block
-  availability_zone = each.value.az
-  map_public_ip_on_launch = true
-  tags = {
-    Name = "public-subnet-${each.value.az}"
-  }
-}
+# #Public Subnet
+# resource "aws_subnet" "public_subnet" {
+#   for_each          = { for subnet in var.public_subnets : subnet.az => subnet }
+#   vpc_id            = aws_vpc.automata.id
+#   cidr_block        = each.value.cidr_block
+#   availability_zone = each.value.az
+#   map_public_ip_on_launch = true
+#   tags = {
+#     Name = "public-subnet-${each.value.az}"
+#   }
+# }
 
-#Route Table Associations
-resource "aws_route_table_association" "public" {
-  for_each = aws_subnet.public_subnet
-  subnet_id      = each.value.id
-  route_table_id = aws_route_table.public_route_table.id
-}
+# #Route Table Associations
+# resource "aws_route_table_association" "public" {
+#   for_each = aws_subnet.public_subnet
+#   subnet_id      = each.value.id
+#   route_table_id = aws_route_table.public_route_table.id
+# }
 
